d17fc2cb843ebd90a977854e97551cd6eff1ed6e
Computhink-CS135 <cs135@computhink.com.sg>
2026-01-10 07:32:08 +0000

    lesson 1

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,27 @@
-print("Hello from lesson 3")
+# Task 1
+# age = 9
+# print(age)
+
+# Task 2
+# number1 = 600
+# number2 = 70
+# print(number1 + number2)
+# print(number1 - number2)
+# print(number1 * number2)
+# print(number1 / number2)
+# print(2 + 4 * 5 / 6 + 3 - 6 / 2)
+
+# Task 3
+# 3a
+# a = 50
+# b = 50
+# ans = a + b
+# print(ans)
+
+# 3b
+# ans = (50 - 10)* 3
+# print(ans)
+
+# ans = (100 / 4)
+# print(ans)
+
