a02309b8a8de067ad12f63a1fb976da41a440855
Computhink-CS135 <cs135@computhink.com.sg>
2026-01-17 07:31:39 +0000

    lesson 3

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,3 +1,5 @@
+import math
+
 # Task 1
 # age = 9
 # print(age)
@@ -24,4 +26,49 @@
 
 # ans = (100 / 4)
 # print(ans)
+
+#task 4
+#4a
+# number = 42.56789
+# number = round(number, 2)
+# print(number)
+
+#4b
+# number = 1267.15
+# number = round(number)
+# print (number)
+
+#4c 
+# number = 1785.956
+# number = round(number, -2)
+# print (number)
+
+#task 5
+#5a
+# pineapple_tart = 15 * 2
+# bak_kwa = 58
+# total = pineapple_tart + bak_kwa
+# print(total)
+
+#5b
+# seafood_platter = 96
+# side_dishes = 18 * 3
+# rice = 2 * 6
+# total = (seafood_platter + side_dishes + rice) * 1.1 - 25
+# print(round(total, 2))
+
+#task 6
+#6a
+# movement = -55
+# distance = abs(movement)
+# print distance
+
+#task 7
+# batches = math.floor(9.8)
+# print(batches)
+
+#task 8
+# diameter = 100
+# circumference = math.pi * diameter
+# print(round(circumference, 2))
 
--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,17 @@
-print("Hello from lesson 4")
+
+import math
+
+# task 1
+# red = 1 * 3
+# blue = 2 * 5
+# green = 3 * 4
+# total = red + blue + green
+# print(total)
+
+# task 2
+# name = "eric"
+# print("my name is " + name)
+
+# task 3
+myname = input("whats your name?")
+print(myname + "!")
