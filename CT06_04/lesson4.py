ebfca56e25079a01ada742230419291975d6f2eb
Computhink-CS135 <cs135@computhink.com.sg>
2026-01-24 07:33:32 +0000

    lesson 4

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -13,5 +13,39 @@ import math
 # print("my name is " + name)
 
 # task 3
-myname = input("whats your name?")
-print(myname + "!")
+# myname = input("whats your name?")
+# print(myname + "!")
+
+# task 4
+# 4a
+# number = input("what is your favourite number? ")
+# print("the number",number,"is very intresting")
+
+# 4b
+# name = input("what is your name?\n")
+# print(name + "!")
+
+# task 5
+# 5a
+# name = input("what is your name?")
+# print("Hi," + name + "! nice to meet you")
+
+# 5b
+# hobby = input("what is your fav hobby?")
+# print("I enjoy",hobby,"too!")
+
+# 5c
+# name = input("what is your name?\n")
+# print(name * 10)
+
+# 5d
+# age = int(input("what is your age?\n"))
+# age += 1
+# age = str(age)
+# print("next year, you will be " + age + " years old.")
+
+# 5e
+length = int(input("what is the length?"))
+width = int(input("what is the width?"))
+area = length * width
+print("the area is " + str(area) + ".")
