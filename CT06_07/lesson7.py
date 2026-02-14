777e6b403074a452cfa73ce7a30d02c361456c1c
Computhink-CS135 <cs135@computhink.com.sg>
2026-02-14 07:39:12 +0000

    lesson7

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -24,3 +24,60 @@ import math
 # name = input("name?\n")
 # age = input("age?\n")
 # print(f"hello, {name} you are {age} years old!")
+
+# task1
+# for i in range(1, 10 +1):
+#     print(i)8
+
+# task2
+# for i in range(2, 20 +1, 2):
+#     print(i)
+
+# task3
+# for i in range(10, 0, -1):
+#     print(i)
+
+# task4
+# food = input("what do you like to eat?\n")
+# stop = int(input("how many times do you want to repeat?\n"))
+# for i in range(0, stop):
+#     print(food)
+
+# task5
+# name = input("what is your name?\n")
+# stop = int(input("how many times do you want to repeat?\n"))
+# for i in range(0, stop):
+#     print("nice to meet you, " + name)
+
+# task6
+# num = 0
+# total = 0
+# for i in range(1, 6):
+#     num = int(input(f"what is the number {i}?\n"))
+#     total = num + total
+# print("the total is, " + str(total))
+
+# task7
+# num = int(input("What number for the timestable?\n"))
+# for i in range(1, 13):
+#     print(f"{num} x {i} = {num * i}")
+
+# task8
+# num = int(input("Which number do you choose?\n"))
+# for i in range(1, num +1):
+#     print(f"{str(i) * i}")
+
+# task9
+# total = 0
+# for i in range(1, 6):
+#     num = int(input(f"what is the score {i}?\n"))
+#     total = num + total
+# average = total / 5
+# print(f"the average is, {average}")
+
+# task10
+# num = int(input("Which number do you choose?\n"))
+# for i in range(1, num +1):
+#     print(f"{str(i) * i}")
+# for i in range(num -1, 0, -1):
+#     print(f"{str(i) * i}")
