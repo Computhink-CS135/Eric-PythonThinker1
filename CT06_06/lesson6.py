7c9a65f678c68d4fb6fd1b4091fd7751adf8c553
Computhink-CS135 <cs135@computhink.com.sg>
2026-02-14 06:07:20 +0000

    lesson6 lesson7

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,61 @@
-print("Hello from lesson 6")
+
+import math
+# task1
+# 1a
+# for i in range(3):
+#     print("Hello, World!")
+
+# 1b
+# for i in range(5):
+#     print(i)
+
+# Task 1c
+# print("Hello, World!")
+
+# Task 1d
+# IF = 5
+
+# Task 1e
+# print ("Hello, World!")
+
+# task2
+# 2a
+# age = 10
+# print(age)
+
+# 2b
+# name = "Alice"
+# print(name)
+
+# 2c
+# x = 5
+# print(x)
+
+# 2d
+# print("Hello, World!")
+
+# task3
+# Task 3a
+# age = 25
+# print(age + 1)
+
+# Task 3b
+# number = 10
+# print(number - 5)
+
+# Task 3c
+# print("Repeat" * 3)
+
+# Task 3d
+# year = "2023"
+# print("The year is " + year)
+
+# Task 3e
+# x = 10
+# y = x / 2
+
+# Task 3f
+# end = 5
+# for i in range(end):
+#     print(i)
+
--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,26 @@
-print("Hello from lesson 7")
 
+
+import math
+# recap 1
+# score_one = 80
+# score_two = 90
+# score_three = 75
+# total = score_one + score_two + score_three
+# average_score = total / 3
+# student_name = "Alex"
+# print("Average score for " + student_name + " is: " + str(average_score))
+
+# recap 2
+# total_marks = 0
+# students = input("enter number of students:\n")
+# for i in range(int(students)):
+#     marks = int(input("enter marks:\n"))
+#     total_marks = total_marks + int(marks)
+#     new_total = total_marks + marks
+# print("total: " + str(total_marks))
+# print("average: " + total_marks / int(students))
+
+# recap3
+# name = input("name?\n")
+# age = input("age?\n")
+# print(f"hello, {name} you are {age} years old!")
