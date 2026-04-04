9c2ebe6e40e8ba1df15cd03a4207e16c22d587d2
Computhink-CS135 <cs135@computhink.com.sg>
2026-04-04 07:34:16 +0000

    lesson10 lesson9 lesson9

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,51 @@
-print("Hello from lesson 10")
+
+import math
+# task1
+# num1 = int(input("what is your number?\n"))
+# if num1 >= -1:
+#     print("the number is positive.")
+# else:
+#     print("the number is negative.")
+
+# task2
+# age = int(input("what is your age?\n"))
+# if age <= 13:
+#     print("typical child")
+# else:
+#     if age <= 19:
+#         print("typical teen")
+#     else:
+#         print("typical adult")
+
+# task3
+# degrees = int(input("how many degrees is it outside today?\n"))
+# if degrees >= 30:
+#     print("go swimming")
+# elif degrees >= 25:
+#     print("play basketball")
+# elif degrees >= 20:
+#     print("go cycling")
+# else:
+#     print("read indoors")
+
+# task4
+# grade = int(input("what is your grade?\n"))
+# if grade >= 90:
+#     print("you got an A")
+# elif grade >= 80:
+#     print("you got an B")
+# elif grade >= 70:
+#     print("you got an C")
+# else:
+#     print("you got an F")
+
+# task5
+# money = int(input("how much money do you have?\n"))
+# if money >= 150:
+#     print("you can buy a gaming keyboard.")
+# elif money >= 100:
+#     print("you can buy a new video game.")
+# elif money >= 50:
+#     print("you can buy a gaming mouse.")
+# else:
+#      print("you can buy some snacks.")
--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,54 @@
-print("Hello from lesson 11")
+
+import math
+# task1
+# rider1 = 150
+# rider2 = 125
+# if rider1 >= 120 and rider2 >= 120:
+#     print("both rider1 and 2 can go in")
+# else:
+#     print("both rider1 and 2 cannot go in")
+
+# task2
+# name1 = input("enter your first name").lower()
+# name2 = input("enter your second name").lower()
+# if name1 == "james" and name2 == "leong":
+#     print("YOU ARE WANTED!")
+
+# task3
+# rider1 = 25
+# rider2 = 6
+# if rider1 >= 18 or rider2 >= 18:
+#     print("you are both allowed to ride.")
+# else:
+#     print("you are both not allowed to ride.")
+
+# task4
+# age = int(input("how old are you?\n"))
+# if age <= 12 or age >= 65:
+#     print("ticket price is $100")
+# else:
+#     print("ticket price $20000")
+
+# task5
+# gen = input("enter your gender.\n").lower()
+# if gen == "m" or gen == "male":
+#     print("valid input")
+# else:
+#     print("invalid input")
+
+# task6
+# pw = input("what is the password?\n").lower()
+# if not pw == "passme":
+#     print("password is incorrect")
+# else:
+#     print("password is correct")
+
+# task7
+# username = "i'm soo gay"
+# pw = "i'm a alien"
+# username1 = input("what is the username?\n").lower()
+# pw1 = input("what is the password?\n").lower()
+# if username1 == username and pw1 == pw:
+#     print("access granted.")
+# else:
+#     print("access denied.")
